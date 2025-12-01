692d769794f0fcdf75db56733ac0c9596c1fd6f3
chaitanya-reddy-13 <cs22b1052@iiitdm.ac.in>
2025-12-01 22:27:17 +0530

    Changes in index.html

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Mount the SPA entry point as soon as the root element is available.
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
